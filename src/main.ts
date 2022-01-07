8affd1ee168f8ab30dcb7448fa5d64812c567643
Dipanjan De <dipanjan131@gmail.com>
2022-01-08 03:47:06 +0530

    Removed vue-meta

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import { store } from "./store";
 import App from "./App.vue";
-import { createMetaManager } from "vue-meta";
 
 const app = createApp(App);
 app.use(store);
-app.use(createMetaManager());
 app.provide("$store", store);
 app.mount("#app");
